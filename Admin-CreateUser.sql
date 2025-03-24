6dffdf27a4db3227879ee1eb7730c7b0d38da336
Shubham Bagwe <bagwe.sh@northeastern.edu>
2025-03-24 13:12:48 -0400

    Only app admin user script

--- Admin-CreateUser.sql
+++ Admin-CreateUser.sql
@@ -33,29 +33,4 @@ GRANT CREATE ANY CONTEXT TO BikeUser;
 GRANT EXECUTE ON DBMS_SESSION TO BikeUser;
 GRANT EXECUTE ON DBMS_RLS TO BikeUser;
 
---------------------------------------------------------------------------------
--- Drop and Create DataViewerUser
---------------------------------------------------------------------------------
-BEGIN
-    EXECUTE IMMEDIATE 'DROP USER DataViewerUser CASCADE';
-EXCEPTION
-    WHEN OTHERS THEN
-        NULL; -- Ignore if the user doesn't exist
-END;
-/
 
-BEGIN
-    EXECUTE IMMEDIATE 'CREATE USER DataViewerUser IDENTIFIED BY ViewOnly2025#';
-    DBMS_OUTPUT.PUT_LINE('User DataViewerUser created successfully.');
-EXCEPTION
-    WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Error creating user DataViewerUser: ' || SQLERRM);
-END;
-/
-
-ALTER USER DataViewerUser DEFAULT TABLESPACE users QUOTA 0 ON users;
-ALTER USER DataViewerUser TEMPORARY TABLESPACE temp;
-
--- Grant read-only privileges
-GRANT CREATE SESSION TO DataViewerUser;
-GRANT SELECT ANY TABLE TO DataViewerUser;
