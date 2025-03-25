49c01666d609cf84abb8f2ddd6fbc52227f704bc
Shubham Bagwe <bagwe.sh@northeastern.edu>
2025-03-24 20:21:45 -0400

    Grant data Viewer

---
+++ BikeUser-Grant DataViewerUser.sql
@@ -0,0 +1,105 @@
+--Grant read-only access to DataViweruser
+
+CREATE OR REPLACE VIEW Customer_View_Limited AS
+SELECT 
+    Customer_ID, 
+    First_Name, 
+    Last_Name, 
+    Email, 
+    Phone, 
+    City, 
+    State_Code
+FROM 
+    BikeUser.Customer;
+    
+CREATE OR REPLACE VIEW Employee_View_Limited AS
+SELECT 
+    Employee_ID, 
+    First_Name, 
+    Last_Name, 
+    Email, 
+    Phone, 
+    City, 
+    State_Code
+FROM 
+    BikeUser.Employee;    
+
+DECLARE
+    table_not_exist EXCEPTION;
+    PRAGMA EXCEPTION_INIT(table_not_exist, -942); -- Error code for "table or view does not exist"
+BEGIN
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON Customer_View_Limited TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON Employee_View_Limited TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Payment_Details TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Bike_Model TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.DOCKS TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Accessory TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Bike TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Maintenance TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Bike_Accessory TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+
+    BEGIN
+        EXECUTE IMMEDIATE 'GRANT SELECT ON BikeUser.Rental TO DataViewerUser';
+    EXCEPTION
+        WHEN table_not_exist THEN
+            NULL;
+    END;
+END;
+/
+
+
+
+
